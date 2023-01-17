d8115b3d6cb9fd1c46fc804e17c143563276bc90
Felipe Schappo <felipeschappoo@gmail.com>
2023-01-16 23:52:27 -0300

    frist commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+import Fastify from 'fastify';
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get('/', async (request, reply) => {
+  const habits = await prisma.habit.findMany();
+  console.log(habits)
+  return { habits };
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running');
+});
